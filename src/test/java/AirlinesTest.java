67681652af7fdfb4b23eae663aecb4dd7cc9d7b8
ankdy7 <ankur.dey.008@gmail.com>
2022-12-30 00:14:28 +0530

    new id

--- src/test/java/AirlinesTest.java
+++ src/test/java/AirlinesTest.java
@@ -14,7 +14,7 @@ public class AirlinesTest {
 
 		String endPoint = "https://api.instantwebtools.net/v1/airlines";
 		Map<String, Object> requestPayload =  
-				Payloads.createAirlinePayloadFromMap("21321312121231", "ABCderasd Airlines", "IN", "ABC", 
+				Payloads.createAirlinePayloadFromMap("213245", "ABCderasd Airlines", "IN", "ABC", 
 						"ABC Slogan", "Mumbai", "abc.com", "2022");
 		
 		Response  response = RestUtils.performPost(endPoint, requestPayload, new HashMap<String, String>());
